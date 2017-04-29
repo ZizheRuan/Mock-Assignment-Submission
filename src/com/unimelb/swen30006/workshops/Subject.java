bedea8d49640a3b6effd6e3cd223432bc6dec39f
ZizheRuan <ruanzizhe@live.com>
2017-04-30 02:20:39 +1000

    2
    2

--- src/com/unimelb/swen30006/workshops/Subject.java
+++ src/com/unimelb/swen30006/workshops/Subject.java
@@ -5,7 +5,7 @@ public class Subject {
 	public String name;
 	public String subjectCode;
 	
-	public Subject(String name, String subjectCode){
+	public Subject(String name,String subjectCode){
 		this.name = name;
 		this.subjectCode = subjectCode;
 	}
--- src/com/unimelb/swen30006/workshops/Test.java
+++ src/com/unimelb/swen30006/workshops/Test.java
@@ -3,9 +3,11 @@ package com.unimelb.swen30006.workshops;
 public class Test {
 
 	public static void main(String[] args) {
-		Staff lecturer = new Staff("Tio", "MIT");
+		Staff lecturer = new Staff("Tio", 111111,"MIT");
 		Student studentA = new Student("JIA","182838","MIT");
 		Student studentB = new Student("YI","48586","MIS");
+		
+		Subject subjectooo = new Subject("Tio","MIT");
 		
 		lecturer.creatSubject();
 		lecturer.creatAssignment();
